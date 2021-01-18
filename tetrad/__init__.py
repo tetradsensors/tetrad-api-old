ebc202060017549d23cc5021475313d665593226
samycharas <62038402+samycharas@users.noreply.github.com>
2021-01-18 16:22:34 +0100

    Adding CORS module
    Allows www.tetradsensors.com to call the API from the client side ( JS ).

--- tetrad/__init__.py
+++ tetrad/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_caching import Cache
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from flask_cors import CORS, cross_origin
 from firebase_admin import initialize_app
 from tetrad import utils 
 from os import getenv, environ
@@ -16,9 +17,13 @@ logging.error("Inside __init__.py setup")
 
 # app = Flask(__name__)
 app = Flask(__name__, subdomain_matching=True)
+cors = CORS(app, ressources={r"/request_historical*": {"origins": "*"}})
+#Below Route to be tested once we get the API running
+#cors = CORS(app, ressources={r"/request_historical*": {"origins": "https://www.tetradsensors.com/*"}})
 app.config['SERVER_NAME'] = getenv('DOMAIN_NAME')
 app.config["CACHE_TYPE"] = "simple"
 app.config["CACHE_DEFAULT_TIMEOUT"] = 1
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 init(app)
